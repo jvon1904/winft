0c7fb19935d084b3d8b1f70eca0fb04e712f5e88
Jesse vonBergen <jvonbergen@brotherhoodmutual.com>
2024-06-14 10:15:25 -0400

    rubocop corrections

--- test/test_winft.rb
+++ test/test_winft.rb
@@ -7,15 +7,13 @@ class TestWinft < Minitest::Test
     refute_nil ::Winft::VERSION
   end
 
-  def test_that_it_starts_1601
+  def test_starting_epoch
     assert_equal Time.at_winft(0), Time.utc(1601).localtime
+    assert_equal Time.at_winft("0"), Time.utc(1601).localtime
   end
 
-  def test_that_it_starts_1601_string
-    assert_equal Time.at_winft('0'), Time.utc(1601).localtime
-  end
-
-  def test_that_it_measures_100_nanoseconds
+  def test_measures_100_nanoseconds
     assert_equal Time.at_winft(10_000_000), Time.utc(1601, 1, 1, 0, 0, 1).localtime
+    assert_equal Time.at_winft("10000000"), Time.utc(1601, 1, 1, 0, 0, 1).localtime
   end
 end
